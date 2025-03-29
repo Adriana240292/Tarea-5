1b2f1a810d1032e144a159f7768e475ce1d0110c
Adriana240292 <jcatalan@miumg.edu.gt>
2025-03-29 15:10:15 -0600

    Punteros Dinamicos

--- PunterosDinamicos.cpp
+++ PunterosDinamicos.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
 using namespace std;
+// punteros con asignacion de memoria dinamica;
+// new = reservar un espacio en memoria
+// delete [] = liberar la memoria
+// recolector de basura
 
 main(){
 	/*int edad,*p_edad;
@@ -15,13 +18,15 @@ main(){
 		cout<<"Menor de edad"<<endl;
 	}*/
 	
-	int total = 0;
-	int notas[total];
-	char res;
+	/*int total = 0,*p_notas;
+	p_notas = new int[total];
+	/*int notas[total];*/
+	/*char res;
+	/*int *p_notas = notas;*/
 	
-	do{
+	/*do{
 		cout<<"Ingrese nota "<<total<<": ";
-		cin>>notas[total];
+		cin>>p_notas[total];
 		total++;
 		cout<<"Desea ingresar otro valor (s/n) : ";
 		cin>>res;
@@ -29,11 +34,45 @@ main(){
 	
 	cout<<"-----------Mostrar notas-----------"<<endl;
 	for (int i=0; i<total;i++){
-		cout<<"Nota("<<i<<"): "<<notas[i]<<endl;
+		cout<<"Nota("<<i<<"): "<<*p_notas<<endl;
+		p_notas++;
+	}
+	delete[] p_notas;*/
+	
+	int fil = 0, col=0,**pm_notas;
+	cout<<"Ingrese la cantidad Estudiantes: ";
+	cin>>fil;
+	cout<<"Ingrese la cantidad Notas por estudiante: ";
+	cin>>col;
+	pm_notas = new int *[fil];
+	for(int i=0;i<fil;i++){
+		pm_notas[i] = new int [col];
+	}
+	for (int i=0;i<fil;i++){
+		cout<<"____________Estudiante____________"<<i<<endl;
+		for (int ii=0;ii<col;ii++){
+			cout<<"Nota:"<<ii<<" : ";
+			cin>>*(*(pm_notas+i)+ii);
+			
+			}
+			cout<<"_________________________"<<endl;
+	}
+	cout<<"--------Mostrar Notas --------"<<endl;
+	for (int i=0;i<fil;i++){
+	
+		
+		for (int ii=0;ii<col;ii++){
+		cout<<*(*(pm_notas+i)+ii)<<endl;
+	}
+	cout<<"_______________________________"<<endl;
 	}
 	
+	for(int i=0;i<fil;i++){
 	
-	
+		delete [] pm_notas[i];
+	}
+	delete [] pm_notas;
+		
 	system("pause");
 	
 }
